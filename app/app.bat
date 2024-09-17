251c6f7c8b2fd9b2edf83d37ef438d7fc45d1aaa
Dusan Paal <dusanpaal@gmail.com>
2024-09-17 16:24:41 +0200

    Absolute paths changed to relative.

--- app/app.bat
+++ app/app.bat
@@ -1,2 +1,2 @@
 ECHO %*
-C:\bia\ledvance_ar_payment_advice_converter\prod\app\env\Scripts\python.exe C:\bia\ledvance_ar_payment_advice_converter\prod\app\app.py --email_id %*
+env\Scripts\python.exe app\app.py --email_id %*
